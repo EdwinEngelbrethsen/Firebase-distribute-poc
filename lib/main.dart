cd3e3efe83934d5352c7565b40538f076ea384a5
EdwinEngelbrethsen <Edwin_engelbrethsen@outlook.com>
2025-02-11 08:34:46 +0100

    flutter gradle upgrade

--- lib/main.dart
+++ lib/main.dart
@@ -106,9 +106,6 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'This is a test text'
-            ),
-            const Text(
               'You have pushed the button this many times:',
             ),
             Text(
